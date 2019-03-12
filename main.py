6b1e655305bda74d01520a2365ac386ccc0a006d
David <dajohnson1998@gmail.com>
2019-03-11 22:58:56 -0400

    Added buildpack... I think

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
+#app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app) #Veerrryy similar to express, but not quite
 
 @app.route('/')
 def sessions():
     return render_template('session.html')
 
-@app.route('/chat2')
+@app.route('/chat2') #http route header ex. localhost:5000/chat2
 def secondSession():
-    return render_template('chat2.html')
+    return "chat2"
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
-@socketio.on('my event')
+@socketio.on('my event') #when 'my event' event is returned from the client
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    print('received my event: ' + str(json)) # console log never hurt anyone
+    socketio.emit('my response', json, callback=messageReceived) #emmitting a response to the client from the server, returning mesage was recieved!!!
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
